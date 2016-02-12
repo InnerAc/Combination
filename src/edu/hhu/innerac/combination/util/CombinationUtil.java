ab270eeec514739b81a9dd8142e7b46488aeb135
InnerAc <anjicun@live.com>
2016-02-12 18:35:36 +0800

    增加了注释

--- src/edu/hhu/innerac/combination/util/CombinationUtil.java
+++ src/edu/hhu/innerac/combination/util/CombinationUtil.java
@@ -2,7 +2,13 @@ package edu.hhu.innerac.combination.util;
 
 public class CombinationUtil {
 
-	static int cnt = 0;
+	/**
+	 * 求排列数<br>
+	 * 从n个数中取t个数，然后按照顺序排成一列。<br>
+	 * @param n	总数
+	 * @param t 待排列的数
+	 * @return	得到的排列数
+	 */
 	public static int A(int n, int t){
 		if (t > n){
 			return -1;
@@ -10,18 +16,28 @@ public class CombinationUtil {
 		int ans = 1;
 		for(int i=0;i<t;i++){
 			ans *= (n--);
-			cnt++;
 		}
 		return ans;
 	}
+	/**
+	 * 求阶乘<br>
+	 * @param n
+	 * @return
+	 */
 	public static int factorial(int n){
 		int ans = 1;
 		for(int i=2;i<=n;i++){
 			ans *= i;
-			cnt++;
 		}
 		return ans;
 	}
+	/**
+	 * 求组合数<br>
+	 * 从n个数中，任取t个数组成一组。<br>
+	 * @param n
+	 * @param t
+	 * @return
+	 */
 	public static int C(int n, int t){
 		int c = n - t;
 		t = t<c?t:c;
@@ -29,7 +45,6 @@ public class CombinationUtil {
 		
 	}
 	public static void main(String args[]){
-		System.out.println(C(10, 8));
-		System.out.println(cnt);
+		System.out.println(C(3, 3));
 	}
 }
